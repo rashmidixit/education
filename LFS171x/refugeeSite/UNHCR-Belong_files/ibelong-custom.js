95f32ee27f13c50ebd2219e822c297f18f5f2d98
RD <rashmi_dixit@accelerite.com>
2019-03-03 19:37:44 +0530

    Added the UN site from where Rahaf will download the app and scan a QR Code.

---
+++ LFS171x/refugeeSite/UNHCR-Belong_files/ibelong-custom.js
@@ -0,0 +1,17 @@
+(function($){
+        $('.targetDiv').hide();
+        $('.showSingle').click(function(){
+              //jQuery('.targetDiv').hide();
+              $('#div'+$(this).attr('id')).slideToggle();
+        });
+})( jQuery );
+
+
+
+
+(function($) {
+	
+$(".et_pb_fullwidth_header_subhead").insertBefore(".header-content-container h1");
+
+
+})( jQuery );
