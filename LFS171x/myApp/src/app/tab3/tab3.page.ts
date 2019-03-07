322708a85eccbb33b9b0c9a7752e5fb06e76b3d7
Shashi Sharma <shashi_sharma@persistent.com>
2019-03-07 19:06:44 +0530

    Integrating finger print and communicating to iframe about the result

--- LFS171x/myApp/src/app/tab3/tab3.page.ts
+++ LFS171x/myApp/src/app/tab3/tab3.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { FingerprintAIO ,FingerprintOptions} from '@ionic-native/fingerprint-aio/ngx';
+
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +9,35 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-  logEvent(event) {
-    window.frames[0].postMessage("FromIframeNew", "*");
+  logEvent(event, message) {
+    window.frames[0].postMessage(message, "*");
   }
+  
+  fingerprintOptions : FingerprintOptions;
+  constructor(private fingerprint: FingerprintAIO,private platform: Platform) {
+    this.fingerprintOptions={
+      clientId: 'fingerprint-demo',
+      clientSecret: 'password',
+      disableBackup: true
+      
+    }
+  }
+  async showFingerprintAuthDlg(){
+    try{
+      await this.platform.ready();
+      const available = await this.fingerprint.isAvailable();
+      console.log(available)
+      if(available === "finger"){
+        const result = await this.fingerprint.show(this.fingerprintOptions);
+        console.log(result);
+        window.frames[0].postMessage(result, "*");
+      }
+    }
+    catch(e)
+    {
+      console.error(e);
+    }
+    
+  }
+  
 }
